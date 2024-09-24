13d4c7df629adb291aa98636099a741f0e05c8da
Sunset Mikoto <26019675+SunsetMkt@users.noreply.github.com>
2024-09-24 22:52:47 +0800

    Upgrade API endpoints and workflows

--- .github/workflows/AutoInviteToOrgByStar.py
+++ .github/workflows/AutoInviteToOrgByStar.py
@@ -1,43 +1,72 @@
-# Max Base
-# 2021-06-19
+# Max Base & SunsetMkt
+# 2021-06-19 & 2024-09
 # https://github.com/BaseMax/AutoInviteToOrgByStar
 
-import os
-import sys
 import json
+import os
+
 import requests
 
-print("Hello, World")
+# print("Hello, World")
 
-if os.getenv('CI'):
-    print('Looks like GitHub!')
+if os.getenv("CI"):
+    print("Looks like GitHub Actions!")
 else:
-    print('Maybe running locally?')
+    print("Maybe running locally?")
 
-print("Environ:")
-print(os.environ)
-print("Prefix:")
-print(sys.prefix)
+# Do not show your secrets in the logs
+# print("Environ:")
+# print(os.environ)
+# print("Prefix:")
+# print(sys.prefix)
 
-MY_GITHUB_KEY = os.environ['MY_GITHUB_KEY']
-COMMUNITY_TEAM_ID = os.environ['COMMUNITY_TEAM_ID']
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+ORG_NAME = os.environ["ORG_NAME"]
+TEAM_NAME = os.environ["TEAM_NAME"]
 
-file = open(os.environ['GITHUB_EVENT_PATH'])
+# GITHUB_EVENT_PATH: The path to the file on the runner that
+# contains the full event webhook payload.
+file = open(os.environ["GITHUB_EVENT_PATH"])
 data = json.load(file)
 
-print("Data:")
-print(data)
+# print("Data:")
+# print(data)
 
-USERNAME = data['sender']['login']
+USERNAME = data["sender"]["login"]
 
-print('Send invite for the @'+USERNAME)
-
-# TODO: check user already joined or no....
-url = 'https://api.github.com/teams/'+COMMUNITY_TEAM_ID+'/memberships/' + USERNAME
-payload=''
 headers = {
-    'Accept': 'application/vnd.github.v3+json',
-    'Authorization': 'token '+MY_GITHUB_KEY
+    "Accept": "application/vnd.github+json",
+    "Authorization": f"Bearer {ACCESS_TOKEN}",
+    "X-GitHub-Api-Version": "2022-11-28",
 }
-response = requests.request("PUT", url, headers=headers, data=payload)
-print(response.text)
+
+print(f"Checking if @{USERNAME} is already in the team ...")
+
+
+response = requests.get(
+    f"https://api.github.com/orgs/{ORG_NAME}/teams/{TEAM_NAME}/memberships/{USERNAME}",
+    headers=headers,
+)
+
+if response.status_code == 200:
+    print(f"User @{USERNAME} is already in the team.")
+    exit(0)
+elif response.status_code == 404:
+    print(f"User @{USERNAME} is not in the team.")
+else:
+    print(f"Error: {response.status_code} {response.text}")
+    exit(1)
+
+print(f"Inviting @{USERNAME} ...")
+
+response = requests.put(
+    f"https://api.github.com/orgs/{ORG_NAME}/teams/{TEAM_NAME}/memberships/{USERNAME}",
+    headers=headers,
+)
+
+if response.status_code == 200:
+    print(f"Invitation sent to @{USERNAME}.")
+    exit(0)
+else:
+    print(f"Error: {response.status_code} {response.text}")
+    exit(1)
